7683490e0f2b2b5653f0f401f7d78c5381019b91
Tennormodz <giddytennor@gmail.com>
2024-08-26 15:00:38 +0300

    Update start.js

--- start.js
+++ start.js
@@ -2,16 +2,16 @@
 /**
 ░█▀▀█ ░█▀▀█ ─█▀▀█ ░█▀▀█ ─█▀▀█ ▀▀█▀▀ ░█─░█ ── ░█▀▄▀█ ░█▀▀▄ 
 ░█▄▄█ ░█▄▄▀ ░█▄▄█ ░█▀▀▄ ░█▄▄█ ─░█── ░█▀▀█ ▀▀ ░█░█░█ ░█─░█ 
-░█─── ░█─░█ ░█─░█ ░█▄▄█ ░█─░█ ─░█── ░█─░█ ── ░█──░█ ░█▄▄▀
 
 
-* @project_name PRABATH-MD
-* @authors Prabath Kumara & sachibot-team(Sachintha) <https://github.com/prabathLK>
+
+* @project_name Savannah-MD
+* @authors Giddy Tennor & Rhodvick Tech-team(accidentallygeniuses) <https://github.com/tennormodz>
 * @description The main hope of creating this bot is to take full advantage of the WhatsApp app and make its work easier
-* @link <https://github.com/prabathLK/PRABATH-MD>
+* @link <https://github.com/tennormodz/savannah->
 * @version 2.1.2
 
-© 2023 Prabath Kumara, SACHI-BOT. All rights reserved.
+© 2023 giddy tennor, Rhodvick-Tech. All rights reserved.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.*/
 
